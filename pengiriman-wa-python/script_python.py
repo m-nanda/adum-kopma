664939de6d0df7ce17aba6a26dcb129aef28d6bf
m-nanda <79885278+m-nanda@users.noreply.github.com>
2021-03-29 22:27:29 +0700

    Create script_python.py
    _Script_ untuk mengirim pesan WA yang dinamis secara otomatis

---
+++ pengiriman-wa-python/script_python.py
@@ -0,0 +1,33 @@
+# Import library yang diperlukan
+import pandas as pd
+import time
+import pyautogui as pg
+import webbrowser as web
+from datetime import datetime
+
+# Mengambil dan meyiapkan data dari file excel
+data = pd.read_excel('tes.xlsx')
+data_dict = data.to_dict('list')
+daftar_nomor = data_dict['nomor']
+isi_pesan = data_dict['pesan']
+combo = zip(daftar_nomor,isi_pesan)
+first = True
+
+# Looping untuk pengiriman pesan
+for nomor, pesan in combo:
+    time.sleep(4)
+    web.open("https://web.whatsapp.com/send?phone="+nomor+"&text="+pesan)
+    if first:
+        time.sleep(6)
+        first=False
+    width,height = pg.size()
+    pg.click(width/2,height/2)
+    time.sleep(8)
+    pg.press('enter')
+    time.sleep(8)
+    pg.hotkey('ctrl', 'w')
+    now = datetime.now()
+    waktu_sekarang = now.strftime("%H:%M:%S")
+    
+    # Untuk keterangan pengiriman
+    print('Pesan dikirim ke ' +nomor +' pada: ' +waktu_sekarang)
